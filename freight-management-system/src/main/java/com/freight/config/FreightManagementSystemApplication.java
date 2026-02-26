5650ec8e026d441f69ecfe1d4f76aba17d3446a8
Lemuel <Lemuel@192.168.1.6>
2026-02-26 23:30:39 +0530

    first code commit

---
+++ freight-management-system/src/main/java/com/freight/config/FreightManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.freight.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreightManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreightManagementSystemApplication.class, args);
+	}
+
+}
---
+++ freight-management-system/src/test/java/com/freight/config/FreightManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.freight.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreightManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
